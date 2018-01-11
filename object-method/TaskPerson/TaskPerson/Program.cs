b61c48522ad3588cdcde86546c922de385507cff
Flameri <arto.lindgren1@gmail.com>
2018-01-11 11:02:13 +0200

    TaskPerson done

--- object-method/TaskPerson/TaskPerson/Program.cs
+++ object-method/TaskPerson/TaskPerson/Program.cs
@@ -7,6 +7,17 @@ namespace TaskPerson
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Person personArto = new Person();
+            personArto.Name = "Arto L";
+            personArto.GrowOld();
+            personArto.PrintPersonInfo();
+
+            Person newPerson = new Person("Joe Doe", 17);
+            newPerson.PrintPersonInfo();
+            newPerson.GrowOld();
+            newPerson.PrintPersonInfo();
+
+            Console.ReadKey();
         }
     }
 }
