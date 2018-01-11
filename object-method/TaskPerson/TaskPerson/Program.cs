aeee22cf6733d6091cd66eeb6ee4cf25cf18a0b8
Flameri <arto.lindgren1@gmail.com>
2018-01-11 11:13:38 +0200

    TaskPerson small changes

--- object-method/TaskPerson/TaskPerson/Program.cs
+++ object-method/TaskPerson/TaskPerson/Program.cs
@@ -9,13 +9,18 @@ namespace TaskPerson
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Person personArto = new Person();
             personArto.Name = "Arto L";
+            Console.WriteLine($"Arton ikä {personArto.GetAge()}");
+
             personArto.GrowOld();
+            Console.WriteLine($"Arton ikä {personArto.GetAge()}");
+            personArto.GrowOld(50);
             personArto.PrintPersonInfo();
 
             Person newPerson = new Person("Joe Doe", 17);
             newPerson.PrintPersonInfo();
             newPerson.GrowOld();
             newPerson.PrintPersonInfo();
+
 
             Console.ReadKey();
         }
