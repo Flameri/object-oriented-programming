36c78faf42af5fed104733387db72eac6cf17920
Flameri <arto.lindgren1@gmail.com>
2018-01-11 09:54:17 +0200

    TaskPerson initialized

---
+++ object-method/TaskPerson/TaskPerson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaskPerson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+        }
+    }
+}
