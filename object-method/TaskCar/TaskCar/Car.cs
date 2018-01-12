553d5fd90c5df5a1dc6476d629d5ed42829a27a0
Flameri <arto.lindgren1@gmail.com>
2018-01-12 11:06:34 +0200

    TaskCar done

---
+++ object-method/TaskCar/TaskCar/Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskCar
+{
+    class Car
+    {
+        //Fields
+        public string Brand;
+        public int Speed;
+
+        //Constructor
+        public Car()
+        {
+            Brand = null;
+            Speed = 0;
+        }
+
+        //Methods
+        public void AskData()
+        {
+            Console.WriteLine($"Anna auton merkki ja nopeus km/h:");
+            Brand = Console.ReadLine();
+            Speed = int.Parse(Console.ReadLine());
+        }
+
+        public void ShowCarInfo()
+        {
+            
+            Console.WriteLine($"Annoit merkiksi {Brand} ja nopeudeksi {Speed} km/h\n");
+        }
+
+        public void Accelerate()
+        {
+            Console.WriteLine("Anna kerroin jolla auton nopeutta lisätään");
+            double speed1 = double.Parse(Console.ReadLine());
+
+            if (speed1 < 1)
+                Console.WriteLine("Negatiivista muutosta ei sallita");
+            else
+                Console.WriteLine($"Nopeutta lisättiin {speed1} kertaiseksi, uusi nopeus on {speed1 * Speed} km/h\n");
+        }
+
+        public void Brake()
+        {
+            double speed2 = Speed * 0.9;
+            Console.WriteLine($"Nopeutta hidastettiin 10%, uusi nopeus on {speed2} km/h\n ------------------\n");
+        }
+
+
+    }
+}
---
+++ object-method/TaskCar/TaskCar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TaskCar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Car newCar = new Car();
+            newCar.AskData();
+            newCar.ShowCarInfo();
+            newCar.Accelerate();
+            newCar.Brake();
+
+            Car newCar2 = new Car();
+            newCar2.AskData();
+            newCar2.ShowCarInfo();
+            newCar2.Accelerate();
+            newCar2.Brake();
+
+            Console.ReadKey();
+        }
+    }
+}
