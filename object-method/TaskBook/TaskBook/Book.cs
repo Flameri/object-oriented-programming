0427da4fa78fb2e671d4a1da54121980ecb14dcc
Flameri <arto.lindgren1@gmail.com>
2018-01-12 08:32:56 +0200

    Started TaskBook

---
+++ object-method/TaskBook/TaskBook/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskBook
+{
+    class Book
+    {
+        //Fields
+        public string Title;
+        public string Author;
+        public int Id;
+        public int Price;
+
+        //Constructor
+        public Book(string Title, string Author, int Id, int Price)
+        {
+            Title = Title;
+            Author = Author;
+            Id = Id;
+            Price = Price;
+        }
+
+        //Methods
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Nimi: {Title}\nKirjoittaja: {Author}\nId: {Id}\nHinta{Price}");
+        }
+
+    }
+}
