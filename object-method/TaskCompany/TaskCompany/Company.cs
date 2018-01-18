dd689be43ff9a22fdc92c6f6fe8851f6b868a586
Flameri <arto.lindgren1@gmail.com>
2018-01-18 08:21:53 +0200

    Started TaskCompany

---
+++ object-method/TaskCompany/TaskCompany/Company.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskCompany
+{
+    class Company
+    {
+        //Fields
+        public string Title;
+        public string Adress;
+        public int Phone;
+        public int Income;
+        public int Expense;
+
+        //Constructor
+        public Company()
+        {
+            Title = " ";
+            Adress = " ";
+            Phone = 0;
+            Income = 0;
+            Expense = 0;
+
+        }
+    }
+}
