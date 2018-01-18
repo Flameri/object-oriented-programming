9ca49513a8d5c357053cffb5757ebae80a36f08c
Flameri <arto.lindgren1@gmail.com>
2018-01-18 10:00:04 +0200

    TaskCompany finished

--- object-method/TaskCompany/TaskCompany/Company.cs
+++ object-method/TaskCompany/TaskCompany/Company.cs
@@ -8,20 +8,61 @@ namespace TaskCompany
     {
         //Fields
         public string Title;
-        public string Adress;
-        public int Phone;
+        public string Address;
+        public string Phone;
         public int Income;
         public int Expense;
 
-        //Constructor
+        //Constructor määritelty parametrit
+        public Company(string title, string address, string phone, int income, int expense)
+        {
+            Title = title;
+            Address = address;
+            Phone = phone;
+            Income = income;
+            Expense = expense;
+        }
+        //Constructor määrittelemätön
         public Company()
         {
-            Title = " ";
-            Adress = " ";
-            Phone = 0;
+            Title = "Ei ole tiedossa";
+            Address = "Ei ole tiedossa";
+            Phone =  "0";
             Income = 0;
             Expense = 0;
+        }
+        // Constructor Kopiointimuodostin
+        public Company(Company company)
+        {
+            Title = company.Title;
+            Address = company.Address;
+            Phone = company.Phone;
+            Income = company.Income;
+            Expense = company.Expense;
+        }
+        //methods 
+        public void CompanyProfit()
+        {
+            double profit = (Income - Expense) / Expense;
 
+            if(profit < 1)
+            {
+                Console.WriteLine("Yrityksellä menee huonosti");
+            }
+            else if(profit > 1 && profit < 3)
+            {
+                Console.WriteLine("Yrityksellä menee tyydyttävästi");
+            }
+            else if(profit >= 3)
+            {
+                Console.WriteLine("Yrityksellä menee hyvin");
+            }
+            Console.WriteLine("-------------------------");
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"\nYrityksen nimi: {Title}\nosoite: {Address}\n" +
+                $"Puhelin nro: {Phone}\nTulot: {Income:C}\nMenot: {Expense:C}\n");
         }
     }
 }
--- object-method/TaskCompany/TaskCompany/Program.cs
+++ object-method/TaskCompany/TaskCompany/Program.cs
@@ -7,6 +7,22 @@ namespace TaskCompany
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Company newCompany = new Company("Diamond Group", "Skinnarilankatu 38", "050 124562", 10000, 2000);
+            Company newCompany2 = new Company(newCompany);
+            Company newCompany3 = new Company();
+            Company newCompany4 = new Company(newCompany3);
+
+
+            newCompany.PrintInfo();
+            newCompany.CompanyProfit();
+            newCompany2.PrintInfo();
+            newCompany2.CompanyProfit();
+            newCompany3.PrintInfo();
+            newCompany4.PrintInfo();
+            
+
+
+            Console.ReadLine();
         }
     }
 }
