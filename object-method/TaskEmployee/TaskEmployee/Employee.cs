3e9a2ce86c9d89e3774b4ec8a9b08d80e718984a
Flameri <arto.lindgren1@gmail.com>
2018-01-18 10:23:11 +0200

    Started TaskEmployee

---
+++ object-method/TaskEmployee/TaskEmployee/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskEmployee
+{
+    class Employee
+    {
+        //Fields
+        public string Name;
+        public int Id;
+        public string Position;
+        public string Salary;
+
+        //Constructor
+        public Employee(string name, int id, string position, string salary)
+        {
+            Name = name;
+            Id = id;
+            Position = position;
+            Salary = salary;
+        }
+        //Methods
+        public void CompareSalary(Employee employee)
+        {
+            if(Salary < employee.Salary)
+            {
+                Console.WriteLine($"{Employee.Name} palkka on pienempi kuin henkilön {employee.Name} palkka");
+            }
+            else
+            {
+                Console.WriteLine($"{employee.Name} palkka on pienempi kuin henkilön {Employee.Name} palkka);
+            }
+
+        }
+    }
+}
---
+++ object-method/TaskEmployee/TaskEmployee/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TaskEmployee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        }
+    }
+}
