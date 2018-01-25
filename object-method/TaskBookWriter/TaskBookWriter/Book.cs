dfd9354531de7c87e338eeb7dc87f367c8769b28
Flameri <arto.lindgren1@gmail.com>
2018-01-25 11:08:22 +0200

    Progress TaskBookWriter

--- object-method/TaskBookWriter/TaskBookWriter/Book.cs
+++ object-method/TaskBookWriter/TaskBookWriter/Book.cs
@@ -6,5 +6,47 @@ namespace TaskBookWriter
 {
     class Book
     {
+        //FIelds
+        public string Name;
+        public string Publisher;
+        public double Price;
+        public static string ThemeName;
+
+
+        //Constructor
+        public Book()
+        {
+            Name = "";
+            Publisher = "";
+            Price = 0;
+            ThemeName = "";
+        }
+
+        public Book(string name, string publisher, double price, string themename)
+        {
+            Name = name;
+            Publisher = publisher;
+            Price = price;
+            ThemeName = themename;
+        }
+
+        //Methods
+        public void GetBook(string name)
+        {
+            //Console.WriteLine("Anna etsimäsi kirjan nimi");
+            //name = Console.ReadLine();
+            if (this.Name == name)
+            {
+                Console.WriteLine($"{Name}, {Publisher}, {Price:C}, {ThemeName}");
+            }
+            else
+            {
+                Console.WriteLine("Kirjaa ei löytynyt");
+            }
+        }
+        public static void  ChangeTheme(string theme)
+        {
+            ThemeName = theme;
+        }
     }
 }
--- object-method/TaskBookWriter/TaskBookWriter/Program.cs
+++ object-method/TaskBookWriter/TaskBookWriter/Program.cs
@@ -7,6 +7,18 @@ namespace TaskBookWriter
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Book book1 = new Book("Digimonit","Otava",29.90,"Jännitys");
+            Book book2 = new Book("La Cosa Nostra", "Otava", 31.10, "Elämänkerta");
+
+            book1.GetBook("Digimonit");
+            book2.GetBook("La Cosa Nostra");
+            book2.GetBook("pellavapää");
+
+            Console.WriteLine(Book.ThemeName);
+            Book.ChangeTheme("Fantasia");
+            Console.WriteLine(Book.ThemeName);
+
+            Console.ReadKey();
         }
     }
 }
