6471c47d59b7c028669973c00bd5adf252334873
Flameri <arto.lindgren1@gmail.com>
2018-02-01 10:56:21 +0200

    progress TaskInheritance

--- object-method/TaskInheritance/TaskInheritance/Program.cs
+++ object-method/TaskInheritance/TaskInheritance/Program.cs
@@ -7,6 +7,12 @@ namespace TaskInheritance
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Transportation Car1 = new Transportation("Farmari", "Volvo", 2000, 4000);
+            Car1.PrintInfo();
+
+            Car car2 = new Car("Sedan", "Toyota", 2005,2000, 4.5, "Rav4",4);
+            car2.PrintInfo();
+            Console.ReadKey();
         }
     }
 }
---
+++ object-method/TaskInheritance/TaskInheritance/Transportation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskInheritance
+{
+    class Transportation
+    {
+        //fields
+        protected string Type;
+        protected string Model;
+        protected int Year;
+        protected double Price;
+
+        //Constructor
+        public Transportation(string type, string model, int year, double price)
+        {
+            Type = type;
+            Model = model;
+            Year = year;
+            Price = price;
+        }
+
+        //methods
+        virtual public void PrintInfo()
+        {
+            Console.WriteLine($"\n{Type}" +
+                $"\n{Model}" +
+                $"\n{Year}\n" +
+                $"{Price:C}");
+        }
+    }
+    class Car : Transportation
+    {
+        //Fields
+        protected double MotorSize;
+        protected string Brand;
+        protected int Doors;
+
+        public Car(string type, string model, int year, double price,double motorsize, string brand, int doors) : base(type, model, year, price)
+        {
+            MotorSize = motorsize;
+            Brand = brand;
+            Doors = doors;
+        }
+
+        //override method
+        public override void PrintInfo()
+        {
+            base.PrintInfo();
+            Console.WriteLine($"{MotorSize}\n{Brand}\n{Doors}");
+        }
+    }
+}
