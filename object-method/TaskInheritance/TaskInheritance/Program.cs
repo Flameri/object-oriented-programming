5a7832db9ede4fe5a793a70ba584fc7917ddcb0a
Flameri <arto.lindgren1@gmail.com>
2018-02-01 09:32:11 +0200

    Started TaskInheritance

---
+++ object-method/TaskInheritance/TaskInheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaskInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+        }
+    }
+}
